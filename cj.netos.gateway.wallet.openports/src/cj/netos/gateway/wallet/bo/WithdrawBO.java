ef7332e1bf6d8f842637301c334629080b569f0b
cj <cj@ss.com>
2020-05-12 23:03:13 +0800

    1.0.0

--- cj.netos.gateway.wallet.openports/src/cj/netos/gateway/wallet/bo/WithdrawBO.java
+++ cj.netos.gateway.wallet.openports/src/cj/netos/gateway/wallet/bo/WithdrawBO.java
@@ -2,11 +2,14 @@ package cj.netos.gateway.wallet.bo;
 
 public class WithdrawBO {
     String witchrawer;
+    String witchrawerName;
     String appid;
+    String currency;
     long amount;
-    String paymentChannelPath;
+    String paymentChannelID;
     long ctime;
-    String memo;
+    String device;
+    String note;
 
     public String getWitchrawer() {
         return witchrawer;
@@ -14,6 +17,14 @@ public class WithdrawBO {
 
     public void setWitchrawer(String witchrawer) {
         this.witchrawer = witchrawer;
+    }
+
+    public String getWitchrawerName() {
+        return witchrawerName;
+    }
+
+    public void setWitchrawerName(String witchrawerName) {
+        this.witchrawerName = witchrawerName;
     }
 
     public String getAppid() {
@@ -24,6 +35,14 @@ public class WithdrawBO {
         this.appid = appid;
     }
 
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
     public long getAmount() {
         return amount;
     }
@@ -32,12 +51,12 @@ public class WithdrawBO {
         this.amount = amount;
     }
 
-    public String getPaymentChannelPath() {
-        return paymentChannelPath;
+    public String getPaymentChannelID() {
+        return paymentChannelID;
     }
 
-    public void setPaymentChannelPath(String paymentChannelPath) {
-        this.paymentChannelPath = paymentChannelPath;
+    public void setPaymentChannelID(String paymentChannelID) {
+        this.paymentChannelID = paymentChannelID;
     }
 
     public long getCtime() {
@@ -46,5 +65,21 @@ public class WithdrawBO {
 
     public void setCtime(long ctime) {
         this.ctime = ctime;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
     }
 }
--- cj.netos.gateway.wallet.program/src/cj/netos/gateway/wallet/ports/WalletTradePorts.java
+++ cj.netos.gateway.wallet.program/src/cj/netos/gateway/wallet/ports/WalletTradePorts.java
@@ -137,130 +137,187 @@ public class WalletTradePorts implements IWalletTradePorts {
     }
 
     @Override
-    public void withdraw(ISecuritySession securitySession, long amount, String paymentChannelPath, String memo) throws CircuitException {
-        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
-                .type("/wallet.ports")
-                .headers(new HashMap() {
-                    {
-                        put("command", "withdraw");
-                        put("device", securitySession.property("device"));
-                        put("person", securitySession.principal());
-                        put("appid", (String) securitySession.property("appid"));
-                    }
-                }).build();
+    public Map<String, Object> withdrawOrder(ISecuritySession securitySession, long amount, String payChannelID, String note) throws CircuitException {
+        if (amount < 0) {
+            throw new CircuitException("500", "金额为负数");
+        }
+        if (StringUtil.isEmpty(payChannelID)) {
+            throw new CircuitException("404", String.format("支付渠道为空"));
+        }
+        Map<String, Object> person = (Map<String, Object>) personService.getPersonInfo((String) securitySession.property("accessToken"));
+        if (person == null) {
+            throw new CircuitException("404", String.format("用户不存在:" + securitySession.principal()));
+        }
+
 
         WithdrawBO withdrawBO = new WithdrawBO();
+        withdrawBO.setWitchrawer((String) person.get("nickName"));
         withdrawBO.setAmount(amount);
         withdrawBO.setAppid((String) securitySession.property("appid"));
         withdrawBO.setCtime(System.currentTimeMillis());
-        withdrawBO.setPaymentChannelPath(paymentChannelPath);
-        withdrawBO.setWitchrawer(securitySession.principal());
-        byte[] body = new Gson().toJson(withdrawBO).getBytes();
+        withdrawBO.setNote(note);
+        withdrawBO.setCurrency("CNY");
+        withdrawBO.setPaymentChannelID(payChannelID);
+        withdrawBO.setDevice((String) securitySession.property("device"));
 
-        rabbitMQ.publish(properties, body);
+        //返回以支付渠道签名的文本
+        return callWithdrawOrder(withdrawBO);
+    }
+
+    private Map<String, Object> callWithdrawOrder(WithdrawBO bo) throws CircuitException {
+        Map<String, Object> mapargs = new HashMap<>();
+        mapargs.put("withdrawBill", bo);
+        String text = new Gson().toJson(mapargs);
+        RequestBody rb = RequestBody.create(text.getBytes());
+
+        OkHttpClient client = (OkHttpClient) site.getService("@.http");
+
+        String appid = site.getProperty("appid");
+        String appKey = site.getProperty("appKey");
+        String appSecret = site.getProperty("appSecret");
+        String portsUrl = site.getProperty("ports.oc.trade");
+        String nonce = Encript.md5(String.format("%s%s", UUID.randomUUID().toString(), System.currentTimeMillis()));
+        String sign = Encript.md5(String.format("%s%s%s", appKey, nonce, appSecret));
+        final Request request = new Request.Builder()
+                .url(portsUrl)
+                .addHeader("Rest-Command", "withdrawOrder")
+                .addHeader("app-id", appid)
+                .addHeader("app-key", appKey)
+                .addHeader("app-nonce", nonce)
+                .addHeader("app-sign", sign)
+                .post(rb)
+                .build();
+        final Call call = client.newCall(request);
+        Response response = null;
+        try {
+            response = call.execute();
+        } catch (IOException e) {
+            throw new CircuitException("1002", e);
+        }
+        if (response.code() >= 400) {
+            throw new CircuitException("1002", String.format("远程访问失败:%s", response.message()));
+        }
+        String json = null;
+        try {
+            json = response.body().string();
+        } catch (IOException e) {
+            throw new CircuitException("1002", e);
+        }
+        Map<String, Object> map = new Gson().fromJson(json, HashMap.class);
+        if (Double.parseDouble(map.get("status") + "") >= 400) {
+            throw new CircuitException(map.get("status") + "", map.get("message") + "");
+        }
+        json = (String) map.get("dataText");
+        map = new Gson().fromJson(json, HashMap.class);
+        return map;
     }
 
     @Override
-    public void payment(ISecuritySession securitySession, long amount, String payee, String type, TradeDetailsBO details) throws CircuitException {
-        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
-                .type("/wallet.ports")
-                .headers(new HashMap() {
-                    {
-                        put("command", "payment");
-                        put("device", securitySession.property("device"));
-                        put("person", securitySession.principal());
-                        put("appid", (String) securitySession.property("appid"));
-                    }
-                }).build();
+    public void withdrawDone(ISecuritySession securitySession, String sn, long amount, String code, String message) throws CircuitException {
 
-        PaymentBO paymentBO = new PaymentBO();
-        paymentBO.setAmount(amount);
-        paymentBO.setAppid((String) securitySession.property("appid"));
-        paymentBO.setCtime(System.currentTimeMillis());
-        paymentBO.setPayee(payee);
-        paymentBO.setPayer(securitySession.principal());
-        paymentBO.setDetails(details);
-        paymentBO.setType(type);
-
-        byte[] body = new Gson().toJson(paymentBO).getBytes();
-
-        rabbitMQ.publish(properties, body);
     }
 
-    @Override
-    public void gathering(ISecuritySession securitySession, long amount, String payer, String type, TradeDetailsBO details) throws CircuitException {
-        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
-                .type("/wallet.ports")
-                .headers(new HashMap() {
-                    {
-                        put("command", "gathering");
-                        put("device", securitySession.property("device"));
-                        put("person", securitySession.principal());
-                        put("appid", (String) securitySession.property("appid"));
-                    }
-                }).build();
-
-        GatheringBO gatheringBO = new GatheringBO();
-        gatheringBO.setAmount(amount);
-        gatheringBO.setAppid((String) securitySession.property("appid"));
-        gatheringBO.setCtime(System.currentTimeMillis());
-        gatheringBO.setPayer(payer);
-        gatheringBO.setPayee(securitySession.principal());
-        gatheringBO.setDetails(details);
-        gatheringBO.setType(type);
-
-        byte[] body = new Gson().toJson(gatheringBO).getBytes();
-
-        rabbitMQ.publish(properties, body);
-    }
-
-    @Override
-    public void transfer(ISecuritySession securitySession, long amount, String payee, TradeDetailsBO details) throws CircuitException {
-        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
-                .type("/wallet.ports")
-                .headers(new HashMap() {
-                    {
-                        put("command", "transfer");
-                        put("device", securitySession.property("device"));
-                        put("person", securitySession.principal());
-                        put("appid", (String) securitySession.property("appid"));
-                    }
-                }).build();
-
-        TransferBO transferBO = new TransferBO();
-        transferBO.setAmount(amount);
-        transferBO.setAppid((String) securitySession.property("appid"));
-        transferBO.setCtime(System.currentTimeMillis());
-        transferBO.setPayee(payee);
-        transferBO.setPayer(securitySession.principal());
-        transferBO.setDetails(details);
-
-        byte[] body = new Gson().toJson(transferBO).getBytes();
-
-        rabbitMQ.publish(properties, body);
-    }
-
-    @Override
-    public void queryAmount(ISecuritySession securitySession) throws CircuitException {
-        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
-                .type("/wallet.ports")
-                .headers(new HashMap() {
-                    {
-                        put("command", "queryAmount");
-                        put("device", securitySession.property("device"));
-                        put("person", securitySession.principal());
-                        put("appid", (String) securitySession.property("appid"));
-                    }
-                }).build();
-
-        QueryAmountBO queryAmountBO = new QueryAmountBO();
-        queryAmountBO.setAppid((String) securitySession.property("appid"));
-        queryAmountBO.setCtime(System.currentTimeMillis());
-        queryAmountBO.setQueryer(securitySession.principal());
-
-        byte[] body = new Gson().toJson(queryAmountBO).getBytes();
-
-        rabbitMQ.publish(properties, body);
-    }
+    //    @Override
+//    public void payment(ISecuritySession securitySession, long amount, String payee, String type, TradeDetailsBO details) throws CircuitException {
+//        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
+//                .type("/wallet.ports")
+//                .headers(new HashMap() {
+//                    {
+//                        put("command", "payment");
+//                        put("device", securitySession.property("device"));
+//                        put("person", securitySession.principal());
+//                        put("appid", (String) securitySession.property("appid"));
+//                    }
+//                }).build();
+//
+//        PaymentBO paymentBO = new PaymentBO();
+//        paymentBO.setAmount(amount);
+//        paymentBO.setAppid((String) securitySession.property("appid"));
+//        paymentBO.setCtime(System.currentTimeMillis());
+//        paymentBO.setPayee(payee);
+//        paymentBO.setPayer(securitySession.principal());
+//        paymentBO.setDetails(details);
+//        paymentBO.setType(type);
+//
+//        byte[] body = new Gson().toJson(paymentBO).getBytes();
+//
+//        rabbitMQ.publish(properties, body);
+//    }
+//
+//    @Override
+//    public void gathering(ISecuritySession securitySession, long amount, String payer, String type, TradeDetailsBO details) throws CircuitException {
+//        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
+//                .type("/wallet.ports")
+//                .headers(new HashMap() {
+//                    {
+//                        put("command", "gathering");
+//                        put("device", securitySession.property("device"));
+//                        put("person", securitySession.principal());
+//                        put("appid", (String) securitySession.property("appid"));
+//                    }
+//                }).build();
+//
+//        GatheringBO gatheringBO = new GatheringBO();
+//        gatheringBO.setAmount(amount);
+//        gatheringBO.setAppid((String) securitySession.property("appid"));
+//        gatheringBO.setCtime(System.currentTimeMillis());
+//        gatheringBO.setPayer(payer);
+//        gatheringBO.setPayee(securitySession.principal());
+//        gatheringBO.setDetails(details);
+//        gatheringBO.setType(type);
+//
+//        byte[] body = new Gson().toJson(gatheringBO).getBytes();
+//
+//        rabbitMQ.publish(properties, body);
+//    }
+//
+//    @Override
+//    public void transfer(ISecuritySession securitySession, long amount, String payee, TradeDetailsBO details) throws CircuitException {
+//        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
+//                .type("/wallet.ports")
+//                .headers(new HashMap() {
+//                    {
+//                        put("command", "transfer");
+//                        put("device", securitySession.property("device"));
+//                        put("person", securitySession.principal());
+//                        put("appid", (String) securitySession.property("appid"));
+//                    }
+//                }).build();
+//
+//        TransferBO transferBO = new TransferBO();
+//        transferBO.setAmount(amount);
+//        transferBO.setAppid((String) securitySession.property("appid"));
+//        transferBO.setCtime(System.currentTimeMillis());
+//        transferBO.setPayee(payee);
+//        transferBO.setPayer(securitySession.principal());
+//        transferBO.setDetails(details);
+//
+//        byte[] body = new Gson().toJson(transferBO).getBytes();
+//
+//        rabbitMQ.publish(properties, body);
+//    }
+//
+//    @Override
+//    public void queryAmount(ISecuritySession securitySession) throws CircuitException {
+//        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder()
+//                .type("/wallet.ports")
+//                .headers(new HashMap() {
+//                    {
+//                        put("command", "queryAmount");
+//                        put("device", securitySession.property("device"));
+//                        put("person", securitySession.principal());
+//                        put("appid", (String) securitySession.property("appid"));
+//                    }
+//                }).build();
+//
+//        QueryAmountBO queryAmountBO = new QueryAmountBO();
+//        queryAmountBO.setAppid((String) securitySession.property("appid"));
+//        queryAmountBO.setCtime(System.currentTimeMillis());
+//        queryAmountBO.setQueryer(securitySession.principal());
+//
+//        byte[] body = new Gson().toJson(queryAmountBO).getBytes();
+//
+//        rabbitMQ.publish(properties, body);
+//    }
 
 }
